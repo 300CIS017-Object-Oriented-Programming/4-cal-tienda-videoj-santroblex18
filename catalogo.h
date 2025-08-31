700031de5b263b122f284a0e12b9179f38b5110e
santroblex18 <roblessant2020@gmail.com>
2025-08-30 21:39:20 -0500

    Update catalogo.h

--- catalogo.h
+++ catalogo.h
@@ -1,1 +1,21 @@
+#ifndef CATALOGO_H
+#define CATALOGO_H
+
+#include <string>
+using namespace std;
+
+const int TOTAL_JUEGOS = 12;
+extern const int codigosJuegos[TOTAL_JUEGOS];
+extern const string nombresJuegos[TOTAL_JUEGOS];
+extern const double preciosJuegos[TOTAL_JUEGOS];
+extern const string plataformasJuegos[TOTAL_JUEGOS];
+extern const string categoriasJuegos[TOTAL_JUEGOS];
+
+void mostrarCatalogoJuegos();
+string obtenerNombreJuego(int codigoJuego);
+double obtenerPrecioJuego(int codigoJuego);
+string obtenerPlataforma(int codigoJuego);
+string obtenerCategoria(int codigoJuego);
+
+#endif
 
